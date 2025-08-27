72bb6b8b99ed726be45cf4812a5bfa9b74e0a304
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2025-08-27 17:44:58 +0000

    Factor out default.nix

--- flake.nix
+++ flake.nix
@@ -8,23 +8,11 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        myproject = pkgs.rustPlatform.buildRustPackage {
-          pname = "multi-arch-demo";
-          version = "1.0.0";
-          src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
-        };
-        docker = pkgs.dockerTools.buildLayeredImage {
-          name = "localhost/myproject";
-          config = {
-            Cmd = ["${myproject}/bin/multi-arch-demo"];
-          };
-        };
+        build = pkgs.callPackage ./default.nix {};
       in
       {
-        packages = { inherit myproject docker; };
+        defaultPackage = self.packages.${system}.myproject;
+        packages = { inherit (build) myproject docker; };
         devShell = with pkgs; mkShell {
           buildInputs = [ cargo ];
         };
